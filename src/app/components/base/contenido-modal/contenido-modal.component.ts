c8a0c53f6be2c57542da8c7b957fcf06d24b77ff
MarioGS-tech <sistemadefacturacionelectronica@globalsolutionslt.com>
2025-02-14 10:27:26 -0600

    FRONTEND SF

---
+++ src/app/components/base/contenido-modal/contenido-modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { SharedDataService } from 'src/app/core/services/shared-data-service.service';
+@Component({
+  selector: 'app-contenido-modal',
+  templateUrl: './contenido-modal.component.html',
+})
+export class ContenidoModalComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialog: MatDialog,
+    private sharedDataService: SharedDataService
+  ) {}
+
+  public closeModal() {
+    this.dialog.closeAll();
+    this.sharedDataService.setVariable('UHRI-disabled', false);
+  }
+}
---
+++ src/app/components/base/modal-basico/modalbasico.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-modalbasico',
+  templateUrl: './modalbasico.component.html',
+  styleUrls: ['./modalbasico.component.css'],
+})
+export class ModalBasicoComponent {
+  constructor(
+    public dialog: MatDialog,
+    private dialogRef: MatDialogRef<ModalBasicoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  aceptarDialog() {
+    this.data.callerComponent.aceptarDialog();
+    this.dialogRef.close();
+  }
+
+  closeModal(): void {
+    this.dialog.closeAll();
+  }
+}
